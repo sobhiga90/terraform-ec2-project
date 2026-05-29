8867ba4570598fb9ca0259c223a971fe6241cd47
Sobhi <sobhi.ramasamy@gmail.com>
2026-05-29 11:25:00 +0530

    Reset repo: clean Terraform + CI/CD structure

--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-region         = "ap-south-1"
-instance_type  = "t3.micro"
--- user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install nginx -y
-systemctl start nginx
-systemctl enable nginx
-
-echo "Hello from Terraform EC2 Web Server" > /var/www/html/index.html
